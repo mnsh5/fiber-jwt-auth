1ad898250c54e1aaa4c671ad6c34fb2997620407
mnsh5 <a779433@DESKTOP-5E8HEMG>
2024-03-26 13:16:50 -0300

    :rocket: add new updates

---
+++ src/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/mnsh5/fiber-jwt-auth/src/database"
+)
+
+func main() {
+	database.ConnectDB()
+
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowMethods: "GET, POST, PUT, DELETE",
+		AllowHeaders: "Origin, Content-Type, Accept",
+		// AllowCredentials: true,
+	}))
+
+	log.Fatal(app.Listen(":3000"))
+}
