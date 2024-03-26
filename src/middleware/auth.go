4e959a6645afff7cfdc05a4fd6f490680113c886
mnsh5 <a779433@DESKTOP-5E8HEMG>
2024-03-26 16:08:38 -0300

    :rocket: add new updates

--- src/middleware/auth.go
+++ src/middleware/auth.go
@@ -13,27 +13,26 @@ import (
 
 func AuthMiddleware(c *fiber.Ctx) error {
 	var tokenString string
-
 	authorization := c.Get("Authorization")
+
 	if strings.HasPrefix(authorization, "Bearer ") {
 		tokenString = strings.TrimPrefix(authorization, "Bearer ")
 	}
-
-	// Si no hay token en el encabezado Authorization, intenta obtenerlo de la cookie
-	if tokenString == "" {
+	if c.Cookies("token") != "" {
 		tokenString = c.Cookies("token")
 	}
 
-	// Si no hay token en el encabezado ni en la cookie, permitir que la solicitud continúe
-	if tokenString == "" {
-		return c.Next()
+	if tokenString != "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status":  "fail",
+			"message": "You are not logged in"})
 	}
 
 	// Verificar la validez del token
 	tokenByte, err := jwt.Parse(tokenString, func(jwtToken *jwt.Token) (interface{}, error) {
 		// Verificar el algoritmo de firma
 		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", jwtToken.Header["alg"])
+			return nil, fmt.Errorf("Unexpected signing method: %v", jwtToken.Header["alg"])
 		}
 		// Devolver la clave secreta para validar el token
 		return []byte(config.Config("SECRET_KEY")), nil
@@ -42,32 +41,26 @@ func AuthMiddleware(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"status":  "fail",
-			"message": fmt.Sprintf("Invalid token: %v", err),
-		})
+			"message": fmt.Sprintf("Invalid token: %v", err)})
 	}
 
 	claims, ok := tokenByte.Claims.(jwt.MapClaims)
 	if !ok || !tokenByte.Valid {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"status":  "fail",
-			"message": "Invalid token",
-		})
+			"message": "Invalid token"})
 	}
 
-	// Obtener el usuario asociado con el token
 	var user models.User
 	db := database.DB
 	db.First(&user, "id = ?", fmt.Sprint(claims["sub"]))
 
-	// Verificar si el usuario asociado al token existe
-	if user.ID == 0 {
+	if float64(user.ID) != claims["sub"] {
 		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
 			"status":  "fail",
-			"message": "The user associated with this token does not exist",
-		})
+			"message": "The user associated this token dosn't exists"})
 	}
 
-	// Agregar el usuario a los datos locales del contexto para que esté disponible en los controladores
 	c.Locals("user", &user)
 	return c.Next()
 }
