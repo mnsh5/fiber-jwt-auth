f816a91229141705a31addc7d0675b15d7b63d51
mnsh5 <a779433@DESKTOP-5E8HEMG>
2024-03-26 15:03:50 -0300

    :rocket: add new updates

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ go get github.com/gofiber/fiber/v2
 go get -u gorm.io/gorm
 
 go get -u gorm.io/driver/postgres
+
+go get github.com/golang-jwt/jwt
---
+++ src/services/login.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mnsh5/fiber-jwt-auth/src/database"
+	"github.com/mnsh5/fiber-jwt-auth/src/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func SignIn(c *fiber.Ctx) error {
+	var payload *models.SignInInput
+	db := database.DB
+
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+
+	var user models.User
+	result := db.First(&user, "email = ?", strings.ToLower(payload.Email))
+
+	if result.Error != nil && strings.Contains(result.Error.Error(), "duplicate key value violates unique") {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{
+			"status":  "fail",
+			"message": "The user with the email already exists",
+		})
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": "Invalid email or password"})
+	}
+}
