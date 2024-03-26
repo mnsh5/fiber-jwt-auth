4655b6cbbf875080e9c38a68dd2189404b948796
mnsh5 <a779433@DESKTOP-5E8HEMG>
2024-03-26 13:07:14 -0300

    :rocket: add new models

---
+++ src/models/models.go
@@ -0,0 +1,21 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"type:varchar(100);not null"`
+	Email    string `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Password string `gorm:"type:varchar(100);not null"`
+}
+
+type SignUpInput struct {
+	Username string `json:"username" validate:"required"`
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required, min=8"`
+}
+
+type SignInInput struct {
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
