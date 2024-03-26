3d35f59fba968ae9d18e559b10128d6d2293fa03
mnsh5 <a779433@DESKTOP-5E8HEMG>
2024-03-26 15:48:45 -0300

    :rocket: add new updates

--- src/services/user.go
+++ src/services/user.go
@@ -2,9 +2,13 @@ package services
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/mnsh5/fiber-jwt-auth/src/database"
+	"github.com/mnsh5/fiber-jwt-auth/src/models"
 )
 
 func GetUsers(c *fiber.Ctx) error {
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success"})
+	db := database.DB
+	var users []models.User
+	db.Find(&users)
+	return c.JSON(users)
 }
