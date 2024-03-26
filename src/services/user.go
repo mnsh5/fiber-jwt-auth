65575a6cf04c3b0cca30a1cabe791121e3a85b67
mnsh5 <a779433@DESKTOP-5E8HEMG>
2024-03-26 16:26:48 -0300

    :rocket: add new updates

--- src/services/user.go
+++ src/services/user.go
@@ -6,17 +6,24 @@ import (
 	"github.com/mnsh5/fiber-jwt-auth/src/models"
 )
 
-func User(c *fiber.Ctx) error {
-	user := c.Locals("user").(*models.User)
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"status": "success",
-		"data":   fiber.Map{"user": user},
-	})
-}
-
 func GetUsers(c *fiber.Ctx) error {
 	db := database.DB
 	var users []models.User
 	db.Find(&users)
 	return c.JSON(users)
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	db := database.DB
+	user := new(models.User)
+
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(400).JSON(err)
+	}
+
+	db.Create(&user)
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"status": "success",
+		"data":   user,
+	})
 }
