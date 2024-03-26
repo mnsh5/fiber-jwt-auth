cf9fe6afd128d6dd58e86b3968b6c14f70ea3a8b
mnsh5 <a779433@DESKTOP-5E8HEMG>
2024-03-26 13:36:40 -0300

    :rocket: add new updates

---
+++ src/services/auth.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mnsh5/fiber-jwt-auth/src/database"
+	"github.com/mnsh5/fiber-jwt-auth/src/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func SignUp(c *fiber.Ctx) error {
+	var payload *models.SignUpInput
+	db := database.DB
+
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	newUser := models.User{
+		Username: payload.Username,
+		Email:    strings.ToLower(payload.Email),
+		Password: string(hashedPassword),
+	}
+
+	result := db.Create(&newUser)
+	if result.Error != nil && strings.Contains(result.Error.Error(), "duplicate key value violates unique") {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{"status": "fail", "message": "The user with the email already exists"})
+	}
+	if result.Error != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "error", "message": "Something went wrong"})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": "success", "data": fiber.Map{"user": &newUser}})
+}
